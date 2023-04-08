e6c9f74a55d3942d862f4b2568194eb133e213c7
Sofia Katzman <katzmansof@gmail.com>
2023-04-08 00:53:00 -0400

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Hi! I love design! How's it going?</p>
+    <img src= {image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
